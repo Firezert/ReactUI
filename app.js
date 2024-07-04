e3c58355b7bb7265b086efde1ff0bf5374f685ea
Firezer Teshome <fredzboy263@gmail.com>
2024-07-04 06:57:39 -0700

    app.js updated

---
+++ app.js
@@ -0,0 +1,14 @@
+export const MainApp=()=>{
+    const nameOfTitle= [1,2,3]
+rendor()
+    return(
+        <div>
+            <header>
+                <title>
+                    nameOfTitle[0]
+                </title>
+            </header>
+        </div>
+    )
+}
+//export MainApp();
